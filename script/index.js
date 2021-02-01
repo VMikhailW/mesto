df4eb1b4e6c5c4337b8674382c5a607f5a600b3a
V.Mikhail <V.Mikhail.V@yandex.ru>
2021-02-02 05:00:58 +0600

    Проверочная

---
+++ script/index.js
@@ -0,0 +1,40 @@
+const openButton = document.querySelector('.profile__edit-button');
+const overlay = document.querySelector('.overlay');
+const closeButton = overlay.querySelector('.popup__close-button');
+const userName = document.querySelector('.profile__name');
+const userAbout = document.querySelector('.profile__caption');
+const form = document.forms.edituser;
+const inputName = form.elements.name;
+const inputAbout = form.elements.about;
+const button = document.querySelector('.element__like-button')
+
+function closePopup() {
+  overlay.classList.remove('overlay__active');
+}
+
+function editUserInfo() {
+  userName.textContent = inputName.value;
+  userAbout.textContent = inputAbout.value;
+}
+
+openButton.addEventListener('click', () => {
+  overlay.classList.add('overlay__active');
+});
+
+closeButton.addEventListener('click', closePopup);
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  editUserInfo();
+  closePopup();
+});
+
+overlay.addEventListener('click', (event) => {
+  if (event.target === event.currentTarget) {
+    closePopup();
+  }
+});
+
+button.addEventListener('click', event => {
+  event.target.classList.toggle('element__like-button_active')
+}) 
