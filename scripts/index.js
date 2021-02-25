e20cd5d878e3c9e524c086af175519dae101b0db
V.Mikhail <V.Mikhail.V@yandex.ru>
2021-02-26 03:04:58 +0600

    26022021

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,20 @@
 let openButton = document.querySelector('.profile__edit-button');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__btn_action_close');
-let userName = document.querySelector('.profile__name');
-let userAbout = document.querySelector('.profile__caption');
-let form = document.forms.edituser;
-let inputName = form.elements.name;
-let inputAbout = form.elements.about;
 
+
+const nameProfile = document.querySelector(".profile__name")
+const aboutProfile = document.querySelector(".profile__caption")
+
+const editProfile = document.querySelector('.profile__edit-button')
+const popupEdit = document.querySelector(".popup__container")
+const formEditProfile = popupEdit.querySelector(".popup__form")
+const nameInput = formEditProfile.querySelector("#name-input")
+const aboutInput = formEditProfile.querySelector("#about-input")
 
 
 function closePopup() {
   popup.classList.remove('popup__active');
-}
-
-function editUserInfo() {
-  userName.textContent = inputName.value;
-  userAbout.textContent = inputAbout.value;
 }
 
 openButton.addEventListener('click', () => {
@@ -24,11 +23,22 @@ openButton.addEventListener('click', () => {
 
 closeButton.addEventListener('click', closePopup);
 
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  editUserInfo();
-  closePopup();
-});
 
+function handlePopupEditOpen() {
+  openPopup(popupEdit)
+  nameInput.value = nameProfile.textContent
+  aboutInput.value = aboutProfile.textContent
+  
+}
 
+//Меняем имя и описание профиля
+function handlePopupEditSubmit(evt) {
+  evt.preventDefault()
+  nameProfile.textContent = nameInput.value
+  aboutProfile.textContent = aboutInput.value
+  closePopup(popupEdit)
 
+}
+
+editProfile.addEventListener("click", handlePopupEditOpen)
+formEditProfile.addEventListener("submit", handlePopupEditSubmit)
