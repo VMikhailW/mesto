eec00a35228245e2ca64d34e195fc9410589dc5d
V.Mikhail <V.Mikhail.V@yandex.ru>
2021-05-17 18:30:47 +0300

    17052021

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -60,6 +60,7 @@ export default class FormValidator {
     setInitialState() {
         this._inputList.forEach((input) => {
             this._hideInputError(input);
+            this._toggleSubmitState();
 
         });
     }
